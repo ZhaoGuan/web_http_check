0fc081c6d34147d9f1892521f35455bf1f30be65
ZhaoGuan <guanzhaogz@gmail.com>
2020-08-21 21:03:31 +0800

    支持上传url_data,yml配置文件

--- web/spiders/web.py
+++ web/spiders/web.py
@@ -4,54 +4,22 @@
 import scrapy
 from scrapy_splash import SplashRequest
 import requests
+import yaml
+import os
+
+PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 # docker run -p 8050:8050 -e HTTP_PROXY=http://192.168.84.168:8899  -e HTTPS_PROXY=http://192.168.84.168:8899 -e http_proxy=http://192.168.84.168:8899 -e https_proxy=http://192.168.84.168:8899 scrapinghub/splash
 
 
 def url_data(source="test"):
-    login_url = {
-        "boss": "/admin/login.php",
-        "boss1": "/admin/login.php",
-        "cms": "/ken/login.php",
-    }
-    user_data = {"test": {
-        "boss": {
-            "username": "392",
-            "password": "123456"
-        },
-        "boss1": {
-            "username": "392",
-            "password": "123456"
-        },
-        "cms": {
-            "username": "392",
-            "password": "123456"
-        },
-    }}
-    host_data = {
-        "test": {
-            "boss": "https://test121.meishubao.com",
-            "boss1": "https://test121new.meishubao.com",
-            "cms": "https://jiaoyantest.meishubao.com"
-        },
-    }
-    url_data = {
-        "boss": [
-            "/admin/mybook.html",
-            "/admin/u_userinfo_home.html?id=1798303",
-            "/1v1boss#/ClassBill/index?userid=1798303",
-            "/admin/deane_ducation.html#/DeanEducation",
-            "/admin/u_userinfo.html",
-            "/admin/u_student_list.html?tag_id=all&",
-        ],
-        "boss1": ["/r_reserve2?user=1791256&times="
-                  "/s_user?user=1791256",
-                  "/s_teacher?teacher=373"],
-        "cms": ["/"
-                "/teach_research/educational/series",
-                "/teach_research/label/list"],
-    }
+    with open(PATH + "/../../url_data.yml") as f:
+        data = yaml.safe_load(f)
+    host_data = data["host_data"]
+    url_data = data["url_data"]
+    user_data = data["user_data"]
+    login_url = data["login_url"]
     host = host_data[source]
     url_result = {}
     for key, value in url_data.items():
@@ -90,3 +58,4 @@ class WebSpider(scrapy.Spider):
 
     def net_page(self, response):
         pass
+
